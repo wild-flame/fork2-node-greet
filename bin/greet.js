e5a95258e48231cf6966a650a3c38365ae125ce0
wild-flame <wildflame@126.com>
2014-05-03 19:58:53 +0800

    add command.coffee

--- bin/greet.js
+++ bin/greet.js
@@ -1,10 +1,3 @@
 #!/usr/bin/env node
-
-var greet = require("../lib"),
-    name  = process.argv[2],
-    parseArgs = require('minimist');
-
-// parameters 
-var pars = parseArgs(process.argv)
-
-console.log(greet(name,pars.drunk));
+var command = require("../lib/command");
+command();
